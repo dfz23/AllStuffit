c1c083cb904d8e73a408cf53c559beaac3a66783
dfz-23 <diego.jaramillo@publicar.com>
2017-05-15 06:01:02 -0500

    clasess

---
+++ MilitaryProject/MilitaryProject/MilitaryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryProject
+{
+    /// <summary>
+    /// Componente 
+    /// </summary>
+    abstract class MilitaryModel
+    {
+        
+        int Id { get; set; }
+        string Name { get; set; }
+        int NumMed { get; set; }
+        string Type { get; set; }
+        DateTime AssistTime { get; set; }
+    }
+}
